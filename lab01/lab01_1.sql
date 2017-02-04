5e44f833f68c339938cd95ef7cc4c11ae538f04a
Nate Bender <Nate Bender>
2017-02-04 00:45:54 -0500

    Finished exercise 1.1

--- lab01/lab01_1.sql
+++ lab01/lab01_1.sql
@@ -7,14 +7,20 @@ SELECT COUNT(*) from EMPLOYEES;
 /* list the employees who make more than $15,000 per year */
 SELECT * from EMPLOYEES WHERE SALARY > 15000;
 
-/* list the employees who were hired from 2002–2004 (inclusive) */
+/* list the employees who were hired from 2002ï¿½2004 (inclusive) */
 SELECT * from EMPLOYEES WHERE HIRE_DATE between '01-JAN-02' and '01-JAN-04';
 
-/* list the employees who have a phone number that doesn’t 
+/* list the employees who have a phone number that doesnï¿½t
    have the area code 515 (nb., use NOT LIKE with the wildcard %). */
 SELECT * from EMPLOYEES WHERE PHONE_NUMBER NOT LIKE '515.%%%.%%%';
 
-/* list the names of the employees who are in the finance department. 
-   Try to format the names as “firstname lastname” 
+/* list the names of the employees who are in the finance department.
+   Try to format the names as ï¿½firstname lastnameï¿½
    using concatenation (i.e., || ) and order them alphabetically. */
 SELECT FIRST_NAME, LAST_NAME FROM EMPLOYEES WHERE DEPARTMENT_ID = 100 ORDER BY FIRST_NAME;
+
+/* list the city, state and country name for all locations in the Asian region. */
+SELECT CITY, STATE_PROVINCE, COUNTRY_ID.COUNTRY_NAME FROM LOCATIONS WHERE COUNTRY.COUNTRY_ID.REGION_ID.REGION_NAME = ASIA;
+
+/* list the locations that have no state or province specified in the database */
+SELECT * FROM LOCATIONS where STATE_PROVINCE = NULL;
