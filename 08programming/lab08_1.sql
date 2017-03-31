09e8b6fcb49f7688b0d2a3b8dc63c2c4b4ef4f74
Nate Bender <nsbender1@gmail.com>
2017-03-31 18:35:37 -0400

    Completed lab08_1

--- 08programming/lab08_1.sql
+++ 08programming/lab08_1.sql
@@ -1,33 +1,35 @@
-CREATE OR REPLACE PROCEDURE cast_actor (actorIdIn IN Actor.id%type, movieIdIn IN Movie.id%type, roleIn IN string%type)
+CREATE OR REPLACE PROCEDURE cast_actor (actorIdIn IN Actor.id%type, movieIdIn IN Movie.id%type, roleIn IN varchar2)
 
 AS
 	--DECLARE movieSearch INT DEFAULT -1;
 	actorSearch INT DEFAULT -1;
 	roleCount INT DEFAULT 0;
+	roleActor INT DEFAULT 0;
 	
 BEGIN
 
 	IF actorIdIn IS NULL THEN
-		dbms_output.put_line("Actor Not Found");
+		dbms_output.put_line('Actor Not Found');
 	ELSE
-		dbms_output.put_line("Actor Found. Inserting...");
+		dbms_output.put_line('Actor Found. Inserting...');
 		
 		IF movieIdIn IS NULL THEN
-			dbms_output.put_line("Movie Not Found");
+			dbms_output.put_line('Movie Not Found');
 		ELSE
-			SELECT COUNT(*) INTO roleCount FROM Role WHERE id = movieIdIn;
+			SELECT COUNT(*) INTO roleCount FROM Role WHERE movieId = movieIdIn;
 			IF roleCount < 230 THEN
-				SELECT actorId INTO actorSearch FROM Role WHERE actorId = actorIdIn;
-				IF actorSearch = actorId THEN
-					dbms_output.put_line("Actor already cast in Movie!");
+				SELECT actorId INTO actorSearch FROM Role WHERE Role.actorId = actorIdIn AND Role.movieId = movieIdIn;
+				SELECT Role.actorId INTO roleActor FROM Role WHERE Role.actorId = actorIdIn AND Role.movieId = movieIdIn;
+				IF actorSearch = roleActor THEN
+					dbms_output.put_line('Actor already cast in Movie!');
 				ELSE
-					INSERT INTO Role VALUES (actorId, movieIdIn, roleIn);
+					INSERT INTO Role VALUES (actorIdIn, movieIdIn, roleIn);
+					dbms_output.put_line('Actor Given Role Successfully');			
 				END IF;
 			ELSE
-				dbms_output.put_line("Movie already has 230 roles!");
+				dbms_output.put_line('Movie already has 230 roles!');
 			END IF;
 		END IF;
 	END IF;
-				
 END;
 /
