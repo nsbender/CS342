8d59f2a3f48f13065ae9b11135d010106d830fb9
Nate Bender <nsbender@gmail.com>
2017-05-18 14:57:22 -0400

    updated vision, data, and queries

--- project/queries.sql
+++ project/queries.sql
@@ -1,6 +1,5 @@
 -- The Beer_Basics view returns a summary of each beer for when detailed information is not required.
 -- Additionally, it computes each beers average user score if the beer has more than one score.
-
 CREATE OR REPLACE VIEW Beer_Basics AS
 SELECT DISTINCT b.name, br.name AS BreweryName, b.ibu,(
 	SELECT AVG(score)
@@ -39,6 +38,43 @@ FROM (
 FROM Brewery br, Beer b;
 
 
+-- This Query returns a list of all beers and where they can be purchased. This is useful for
+-- developers to add functionality to their applications that allows users to find where they can
+-- purchase a particular beer.
+SELECT b.name, db.DistributorId 
+FROM Beer b 
+LEFT OUTER JOIN DistributorBeer db
+ON db.beerId = b.id;
 
 
+-- Lists all of the beers in the database an the average price per unit based on all of the
+-- distributors prices averaged. Useful to both buyers for deciding what to purchase in terms
+-- of price per beer, and for distributors to determine pricing.
+SELECT b.name, AVG(db.cost) AS AvgCost
+FROM Beer b LEFT JOIN DistributorBeer db
+ON db.beerId = b.id
+GROUP BY b.name;
 
+
+-- Self JOIN of the Rating table for comparing beers that have both a professional and community
+-- rating. Usefule for brewers to determine how their beer is being received and for consumers
+-- to determine the usefulness of a rating.
+SELECT DISTINCT r1.beerId AS "Beer", r1.score AS "Pro Rating", r2.score AS "Community Rating"
+FROM Rating r1 LEFT JOIN Rating r2
+ON r1.beerId = r2.beerId
+WHERE r1.ratingType = 'p'
+AND r2.ratingType = 'c';
+
+-- Calculates the ratio of breweries with a parent company to that without. Useful in determining
+-- the state of the industry and whether or not a brewery can be started independantly without
+-- risk.
+SELECT (
+	SELECT COUNT(id) FROM BREWERY
+	WHERE parentId IS NOT NULl
+	)/
+	(
+	SELECT COUNT(id) FROM BREWERY
+	WHERE parentId IS NULl
+	)
+AS Ratio
+FROM Dual;
